67bfce63ebf816f62c1255333237b8344c853140
THALICE SILVA <2020101106@academicougb.com.br>
2022-03-23 17:46:21 -0300

    Finalização do projeto SlideShow

--- SlideShow/Slide.js
+++ SlideShow/Slide.js
@@ -1,10 +1,13 @@
+'use strict'; 
 
 const images = [
     {'id': '1', 'url':'img/Flor.jpg'}, 
-    {'id': '2', 'url':'img/Rosa.jpg'}
+    {'id': '2', 'url':'img/Rosa.jpg'},
+    {'id': '3', 'url':'img/Flores.jpg'}
 ];
 
-const container = document.querySelector("#itens");
+const container = document.querySelector('#itens');
+
 const loadImages = (images, container) => {
     images.forEach( image => {
         container.innerHTML += `
@@ -16,3 +19,16 @@ const loadImages = (images, container) => {
 }
 
 loadImages( images, container );
+let items = document.querySelectorAll('.item');
+
+const previous = () => {
+    let items = document.querySelectorAll('.item');
+    container.appendChild(items[0]);
+}
+const next = () => {
+    items = document.querySelectorAll('.item');
+    const lastItem = items[items.length - 1];
+    container.insertBefore( lastItem, items[0]);
+}
+document.querySelector('#anterior').addEventListener('click', previous);
+document.querySelector('#proximo').addEventListener('click', next);
